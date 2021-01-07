083f702d58e71de89d96ffd071b6a2558449ec08
Mariana <moraismariana83@gmail.com>
2021-01-07 14:05:45 -0300

    Feat: Add vuelidate

--- front/src/components/pages/create-employee/CreateEmployeeComponent.vue
+++ front/src/components/pages/create-employee/CreateEmployeeComponent.vue
@@ -5,51 +5,112 @@
         <h3>Add new employee</h3>
       </div>
       <div class="card-body">
-        <form @submit.prevent="handleSubmitForm()">
+        <form v-on:submit.prevent="handleSubmitForm()">
+          <!-- name -->
           <div class="form-group">
             <label class="font-weight-bold">Name</label>
             <input
               type="text"
+              id="name"
+              name="name"
               class="form-control"
               placeholder="Type the name"
-              v-model="employee.name" required>
+              v-model="employeeForm.name"
+              :class="{ 'is-invalid': isSubmitted && $v.employeeForm.name.$error }"
+            />
+            <div
+              v-if="isSubmitted && !$v.employeeForm.name.required"
+              class="invalid-feedback"
+            >
+              'Employee name' field is required!
+            </div>
           </div>
+          <!-- /name -->
 
+          <!-- job -->
           <div class="form-group">
             <label class="font-weight-bold">Job role</label>
             <input
               type="text"
+              id="job_role"
+              name="job_role"
               class="form-control"
               placeholder="Type the job role"
-              v-model="employee.job_role" required>
+              v-model="employeeForm.job_role"
+              :class="{ 'is-invalid': isSubmitted && $v.employeeForm.job_role.$error }"
+            >
+            <div
+              v-if="isSubmitted && !$v.employeeForm.job_role.required"
+              class="invalid-feedback"
+            >
+            'Job role' field is required!
+            </div>
           </div>
+          <!-- /job -->
 
+          <!-- salary -->
           <div class="form-group">
             <label class="font-weight-bold">Salary</label>
             <input
               type="number"
+              id="salary"
+              name="salary"
               class="form-control"
               placeholder="Type the salary"
-              v-model="employee.salary" required>
+              v-model="employeeForm.salary"
+              :class="{ 'is-invalid': isSubmitted && $v.employeeForm.salary.$error }"
+            >
+            <div
+              v-if="isSubmitted && !$v.employeeForm.salary.required"
+              class="invalid-feedback"
+            >
+            'Salary' field is required!
+            </div>
           </div>
+          <!-- /salary -->
 
+          <!-- birth -->
           <div class="form-group">
             <label class="font-weight-bold">Birth</label>
             <input
               type="date"
+              id="birth"
+              name="birth"
               class="form-control"
               placeholder="YYYY/MM/DD"
-              v-model="employee.birth" required>
+              v-model="employeeForm.birth"
+              :class="{ 'is-invalid': isSubmitted && $v.employeeForm.birth.$error }"
+            >
+            <div
+              v-if="isSubmitted && !$v.employeeForm.birth.required"
+              class="invalid-feedback"
+            >
+            'Birth' field is required!
+            </div>
           </div>
+          <!-- birth -->
 
+          <!-- registration -->
           <div class="form-group">
             <label class="font-weight-bold">Registration</label>
             <input
               type="text"
+              id="registration"
+              name="registration"
               class="form-control"
               placeholder="Type the registration"
-              v-model="employee.registration" required>
+              v-model="employeeForm.registration"
+              :class="{ 'is-invalid': isSubmitted &&
+                $v.employeeForm.registration.$error }"
+            >
+            <div
+              v-if="isSubmitted && !$v.employeeForm.registration.required"
+              class="invalid-feedback"
+            >
+            'Registration' field is required!
+            </div>
           </div>
+          <!-- /registration -->
 
           <div class="form-group">
             <button class="btn btn-primary" type="submit">+ Employee</button>
@@ -61,6 +122,9 @@
 </template>
 
 <script>
+
+import { required } from 'vuelidate/lib/validators';
+
 export default {
   components: {
     // eslint-disable-next-line vue/no-unused-components
@@ -68,17 +132,33 @@ export default {
   },
   data() {
     return {
-      employee: {
+      employeeForm: {
         name: '',
         job_role: '',
         salary: '',
         birth: '',
         registration: '',
       },
+      isSubmitted: false,
     };
+  },
+  validations: {
+    employeeForm: {
+      name: { required },
+      job_role: { required },
+      salary: { required },
+      birth: { required },
+      registration: { required },
+    },
   },
   methods: {
     handleSubmitForm() {
+      this.isSubmitted = true;
+
+      this.$v.$touch();
+      if (this.$v.$invalid) {
+        return;
+      }
     },
   },
 };
--- front/src/main.js
+++ front/src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue';
 import VueSweerAlert2 from 'vue-sweetalert2';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/nprogress/nprogress.css';
+import Vuelidate from 'vuelidate';
 import App from './App.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
 
 Vue.use(VueSweerAlert2);
+Vue.use(Vuelidate);
 
 new Vue({
   router,
